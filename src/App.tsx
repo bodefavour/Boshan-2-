3ae5f75122b4dedeb8859c727756097288f86edd
bodefavour <bodefavour@gmail.com>
2025-03-25 13:00:46 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
       <Routes>
         <Route path="/" element={
           <>
-            <HeroSection backgroundImage="/images/HP_Hero-FullBleed-Desktop_Gucci-SOFTBIT-Mar25-GUCCI-PREFALL-DETAILS-29-INDIGO-LEWIN_001_Default.jpg" brandName="BOSHAN" /> <ProductCarousel /> {/* New carousel under the hero section */}
+            <HeroSection backgroundImage="/images/HP_Hero-FullBleed-Desktop_Gucci-SOFTBIT-Mar25-GUCCI-PREFALL-DETAILS-29-INDIGO-LEWIN_001_Default.avif" brandName="BOSHAN" /> <ProductCarousel /> {/* New carousel under the hero section */}
             <ProductGrid products={sampleProducts} />
           </>
         } />
