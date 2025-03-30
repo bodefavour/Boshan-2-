ab9e4e54e6ddb9da5268d641d7b64a5eb2046eab
bodefavour <bodefavour@gmail.com>
2025-03-30 23:27:19 +0100

    Update ProductFilter.tsx

--- src/components/ProductFilter.tsx
+++ src/components/ProductFilter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"; import { AdjustmentsVerticalIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import React, { useState } from "react"; import { AdjustmentsVerticalIcon, XMarkIcon } from "@heroicons/react/outline";
 
 const ProductFilter = ({ onFilterChange }: { onFilterChange: (filters: any) => void }) => { const [filters, setFilters] = useState({ priceRange: "", category: "", sort: "newest", line: "", color: "", material: "", });
 
