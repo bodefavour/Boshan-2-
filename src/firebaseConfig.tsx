22b6da364da5f4c64d54689d1d2683448ff3c9d2
bodefavour <bodefavour@gmail.com>
2025-04-06 23:32:58 +0100

    Create firebaseConfig.tsx

---
+++ src/firebaseConfig.tsx
@@ -0,0 +1,18 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ84wdH3g8q72nczRSrsRSefJJfc196AA",
+  authDomain: "boshan-store.firebaseapp.com",
+  projectId: "boshan-store",
+  storageBucket: "boshan-store.firebasestorage.app",
+  messagingSenderId: "615236015310",
+  appId: "1:615236015310:web:a381d4c12ca6667f316c9f",
+  measurementId: "G-0SVNNJ55PY"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
