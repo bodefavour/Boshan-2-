344b653419a74e23104b892402aaa1585c377b65
Favour Bode <bodefavour@gmail.com>
2025-07-05 17:12:20 +0100

    Update jspdf-autotable.d.ts

--- src/jspdf-autotable.d.ts
+++ src/jspdf-autotable.d.ts
@@ -3,5 +3,10 @@ import "jspdf";
 declare module "jspdf" {
   interface jsPDF {
     autoTable: (options: any) => jsPDF;
+
+    // Add this:
+    lastAutoTable?: {
+      finalY: number;
+    };
   }
 }
