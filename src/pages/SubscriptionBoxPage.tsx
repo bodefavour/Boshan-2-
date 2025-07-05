57cde79d5a9dcf622c07ff02b17009b85aafb933
Favour Bode <bodefavour@gmail.com>
2025-07-05 12:36:40 +0100

    Update SubscriptionBoxPage.tsx

--- src/pages/SubscriptionBoxPage.tsx
+++ src/pages/SubscriptionBoxPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../firebaseConfig";
+import { db } from "../firebaseConfig";
 import { motion } from "framer-motion";
 
 const SubscriptionBoxPage = () => {
