56525092755d52c530ec2ac9312980bd24de6ef6
Favour Bode <bodefavour@gmail.com>
2025-07-05 09:50:34 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,7 @@ function App() {
             <ProductGrid />
           </>
           } />
-<Route path="/preorder/skin-therapy-and-consultations/booking" element={<PrivateRoute> <BookConsultationPage/> </PrivateRoute>} />
+<Route path="/consultation/booking" element={<PrivateRoute> <BookConsultationPage/> </PrivateRoute>} />
           <Route path="/account" element={<PrivateRoute> <AccountPage /> </PrivateRoute>} />
           <Route path="/preorder/skin-therapy-and-consultations" element={<PrivateRoute> <PreorderCategoryPage /> </PrivateRoute>} />
           <Route path="/preorder/skinfood-and-nutrition" element={<PrivateRoute> <PreorderCategoryPage /> </PrivateRoute>} />
